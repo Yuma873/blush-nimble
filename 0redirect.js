599aea98493ccb0fea79a10765731fe146e592aa
Yuma873 <wlnhh22791@yahoo.co.jp>
2026-02-28 08:21:17 +0900

    Update 0redirect.js

--- 0redirect.js
+++ 0redirect.js
@@ -1,5 +1,5 @@
 /* ===== 共通 強制リダイレクトコード ===== */
-const redirectCodeOn = false; // trueにすると全ページで発動
+/*const redirectCodeOn = false; // trueにすると全ページで発動
 
 if (redirectCodeOn) {
   window.location.href = "https://fs.lck-cloud.com/u17540/index/";
